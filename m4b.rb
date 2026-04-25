c08783da13da71d183e8a0f38c4e0f712f436fb1
Alston <alston.albarado@gmail.com>
2026-04-25 00:35:34 +0000

    m4b 1.0.2

--- m4b.rb
+++ m4b.rb
@@ -1,8 +1,8 @@
 class M4b < Formula
   desc "Automated audiobook merging and metadata tagging"
   homepage "https://github.com/alston808/m4b-cli"
-  url "https://github.com/alston808/m4b-cli/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "e6b33f1ce6e58093f5b05b8ad1f3015ae671fb263ffa7f445c441b7b15ba09be" # The action will automatically fix this
+  url "https://github.com/alston808/m4b-cli/archive/refs/tags/v1.0.2.tar.gz"
+  sha256 "54b3602d03f18377568a88aed41e37d3cce22b59d95e4596fa2442e9ebd64290" # The action will automatically fix this
   license "MIT"
 
   depends_on "python@3.12"
